81e9db352c6c9312f21d5471122542a9053bc77c
IsaacYav97 <riq.pacheco@yavirac.edu.ec>
2026-06-04 21:49:32 -0500

    Se agrega del estudiante B

---
+++ app/estudiantes.py
@@ -0,0 +1,16 @@
+def registrar_estudiante(nombre, edad):
+    if nombre == "" or edad <= 0:
+        return "Datos inválidos"
+    return f"Estudiante {nombre} registrado con {edad} años"
+
+
+def calcular_promedio(notas):
+    if len(notas) == 0:
+        return 0
+    return sum(notas) / len(notas)
+
+
+def aprobo(promedio):
+    if promedio >= 7:
+        return "Aprobado"
+    return "Reprobado"
