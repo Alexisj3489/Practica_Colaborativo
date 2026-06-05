be99691629d9718f2465bf04ef14c3ece6863863
IsaacYav97 <riq.pacheco@yavirac.edu.ec>
2026-06-05 08:49:52 -0500

    Cambios en el estudiantes.py

--- app/estudiantes.py
+++ app/estudiantes.py
@@ -12,5 +12,5 @@ def calcular_promedio(notas):
 
 def aprobo(promedio):
     if promedio >= 7:
-        return "Aprobado"
-    return "Reprobado"
+        return "Estudiante aprobado"
+    return "Estudiante reprobado"
